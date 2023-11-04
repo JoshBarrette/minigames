73af38d05fde6bb695d3267dc141d14b6030364e
JoshBarrette <joshpbarrette@gmail.com>
2023-11-03 21:29:44 -0500

    Initial commit

---
+++ src/app/TicTacToe/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css"
+
+export default function TicTacToe() {
+    return (
+        <div className={styles.backgroundDiv}>
+            Tic Tac Toe here
+        </div>
+    );
+};
