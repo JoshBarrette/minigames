f344558e9c25ae69bd9d0a3b6656211cdfe99a0f
JoshBarrette <joshpbarrette@gmail.com>
2023-11-08 00:36:05 -0600

    Finished adding mines and reloading grid with new mines

--- src/app/Minesweeper/page.tsx
+++ src/app/Minesweeper/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import React, { MouseEvent, useEffect, useRef, useState } from "react";
 
 type SquareData = {
     status: number;
@@ -27,44 +27,35 @@ function Square(props: { data: SquareData; gridSize: number }) {
     const clearedAltColor = "bg-green-700";
     const clearedHoverColor = "hover:bg-green-900";
 
+    const failColor = "bg-red-500";
+    const failHoverColor = "hover:bg-red-700";
+
     let bgColor = defaultColor;
     let hoverColor = defaultHoverColor;
 
     // Gotta get that checkerboard effect
-    let isEvenRow = Math.floor((props.data.index / props.gridSize) % 2) === 0;
+    const isEvenRow = Math.floor((props.data.index / props.gridSize) % 2) === 0;
+    const isEvenGrid = props.gridSize % 2 === 0;
     if (props.data.status === squareStatus.cleared) {
         bgColor = clearedColor;
         hoverColor = clearedHoverColor;
 
-        if (props.gridSize % 2 === 1 && props.data.index % 2 === 1) {
+        if (!isEvenGrid && props.data.index % 2 === 1) {
             bgColor = clearedAltColor;
-        } else if (
-            props.gridSize % 2 === 0 &&
-            isEvenRow &&
-            props.data.index % 2 === 1
-        ) {
+        } else if (isEvenGrid && isEvenRow && props.data.index % 2 === 1) {
             bgColor = clearedAltColor;
-        } else if (
-            props.gridSize % 2 === 0 &&
-            !isEvenRow &&
-            props.data.index % 2 === 0
-        ) {
+        } else if (isEvenGrid && !isEvenRow && props.data.index % 2 === 0) {
             bgColor = clearedAltColor;
         }
+    } else if (props.data.status === squareStatus.mine) {
+        bgColor = failColor;
+        hoverColor = failHoverColor;
     } else {
-        if (props.gridSize % 2 === 1 && props.data.index % 2 === 1) {
+        if (!isEvenGrid && props.data.index % 2 === 1) {
             bgColor = defaultAltColor;
-        } else if (
-            props.gridSize % 2 === 0 &&
-            isEvenRow &&
-            props.data.index % 2 === 1
-        ) {
+        } else if (isEvenGrid && isEvenRow && props.data.index % 2 === 1) {
             bgColor = defaultAltColor;
-        } else if (
-            props.gridSize % 2 === 0 &&
-            !isEvenRow &&
-            props.data.index % 2 === 0
-        ) {
+        } else if (isEvenGrid && !isEvenRow && props.data.index % 2 === 0) {
             bgColor = defaultAltColor;
         }
     }
@@ -73,24 +64,26 @@ function Square(props: { data: SquareData; gridSize: number }) {
         <div
             className={`flex h-8 w-8 ${bgColor} p-2 text-xs hover:transition-all ${hoverColor}`}
         >
-            {props.data.status < 2 ? (
+            {props.data.status === squareStatus.flagged ||
+            props.data.status === squareStatus.mine ? (
                 <div className="m-auto select-none">
-                    {/* {squareImages[props.data.status]} */}
+                    {squareImages[props.data.status]}
                 </div>
             ) : null}
-            <p
-                className={
-                    (props.data.isBomb ? "text-red-400" : "text-yellow-400") + " m-auto"
-                }
-            >
-                {props.data.nearCount}
-            </p>
+
+            {props.data.status === squareStatus.cleared &&
+            props.data.nearCount !== 0 ? (
+                <p className="m-auto select-none text-black">
+                    {props.data.nearCount}
+                </p>
+            ) : null}
         </div>
     );
 }
 
 export default function Minesweeper() {
     const [gridSize, setGridSize] = useState(10);
+    const [flagCount, setFlagCount] = useState((gridSize * gridSize) / 5);
     const [grid, setGrid] = useState(
         new Array<SquareData | null>(gridSize * gridSize)
             .fill(null)
@@ -110,8 +103,13 @@ export default function Minesweeper() {
         document.title = "Minesweeper";
     }, []);
 
+    // Need this to stop a race condition :woozy:
+    useEffect(() => {
+        addMines((gridSize * gridSize) / 5);
+    }, [grid]);
+
     const buttonStyling =
-        "transition-all my-1 rounded-md bg-gray-200 px-2 py-1 mx-3 font-sans text-xl shadow hover:bg-gray-400 active:bg-gray-800 active:text-white";
+        "transition-all my-1 rounded-md bg-gray-200 px-2 py-1 mx-3 font-sans text-xl shadow hover:bg-gray-400 active:bg-gray-800 active:text-white ";
 
     function resizeGrid(newSize: number) {
         if (newSize === gridSize) {
@@ -124,6 +122,7 @@ export default function Minesweeper() {
 
     function makeNewGrid(newSize: number) {
         bombCount.current = 0;
+        setFlagCount((newSize * newSize) / 5);
         setGrid(
             new Array<SquareData | null>(newSize * newSize)
                 .fill(null)
@@ -136,25 +135,31 @@ export default function Minesweeper() {
                     };
                 })
         );
+        setGridSize(newSize);
     }
 
-    function addMine(numberOfMines: number) {
+    function addMines(numberOfMines: number) {
         if (numberOfMines < 0) {
             return;
         }
-        
+
         for (let i = 0; i < numberOfMines; i++) {
+            console.log("adding a mine");
             addSingleMine();
         }
+
+        setFlagCount(numberOfMines);
     }
 
     function addSingleMine() {
+        if (bombCount.current === gridSize * gridSize) {
+            return;
+        }
+
         let num = Math.floor(Math.random() * (gridSize * gridSize));
         while (grid[num].isBomb === true) {
-            if (bombCount.current === gridSize * gridSize) {
-                return;
-            }
             num = Math.floor(Math.random() * (gridSize * gridSize));
+            console.log("in the while loop here");
         }
 
         // top row then bottom row
@@ -188,43 +193,59 @@ export default function Minesweeper() {
         }
 
         grid[num].isBomb = true;
-        grid[num].status = squareStatus.mine;
         bombCount.current++;
-
-        // TODO: remove when done
-        setShouldRefresh(!shouldRefresh);
     }
 
     // TODO: need to have this function cascade to all nearby squares with 0 nearby mines
     function handleSquareClick(index: number) {
-        // if (grid[index].status === squareStatus.cleared) {
-        //     return;
-        // } else if (grid[index].isBomb === true) {
-        //     grid[index].status = squareStatus.mine;
-        //     setShouldRefresh(!shouldRefresh);
-        //     return;
-        // }
-
-        if (grid[index].status === squareStatus.cleared) {
-            grid[index].status = squareStatus.flagged;
-        } else {
-            grid[index].status++;
+        if (
+            grid[index].status === squareStatus.cleared ||
+            grid[index].status === squareStatus.flagged
+        ) {
+            return;
+        } else if (grid[index].isBomb === false) {
+            grid[index].status = squareStatus.cleared;
+            setShouldRefresh(!shouldRefresh);
+            return;
+        } else if (grid[index].isBomb === true) {
+            grid[index].status = squareStatus.mine;
+            setShouldRefresh(!shouldRefresh);
+            return;
         }
 
         setShouldRefresh(!shouldRefresh);
+    }
+
+    function handleSquareRightClick(index: number, event: MouseEvent) {
+        event.preventDefault();
+
+        if (grid[index].status === squareStatus.notClicked) {
+            grid[index].status = squareStatus.flagged;
+            setFlagCount(flagCount - 1);
+        } else if (grid[index].status === squareStatus.flagged) {
+            grid[index].status = squareStatus.notClicked;
+            setFlagCount(flagCount + 1);
+        }
     }
 
     return (
         <div className="flex h-screen text-center">
             <div className="m-auto block">
                 <p className="select-none text-5xl font-medium">Minesweeper</p>
-                <br />
 
+                <p className="mb-1">
+                    {squareImages[squareStatus.flagged]}: {flagCount}
+                </p>
                 <div
                     className={`m-auto mb-4 grid w-fit my-grid-cols-${gridSize}`}
                 >
                     {grid.map((square, key) => (
-                        <div onClick={() => handleSquareClick(key)}>
+                        <div
+                            onClick={() => handleSquareClick(key)}
+                            onContextMenu={(e) =>
+                                handleSquareRightClick(key, e)
+                            }
+                        >
                             <Square
                                 data={square}
                                 key={key}
@@ -235,22 +256,24 @@ export default function Minesweeper() {
                 </div>
 
                 <button
-                    className={buttonStyling + " mt-3"}
-                    onClick={() => makeNewGrid(gridSize)}
+                    className={buttonStyling + "mt-3"}
+                    onClick={() => {
+                        makeNewGrid(gridSize);
+                    }}
                 >
                     Reset Game
                 </button>
                 <button
-                    className={buttonStyling + " mt-3"}
+                    className={buttonStyling + "mt-3"}
+                    onClick={() => addMines((gridSize * gridSize) / 5)}
+                >
+                    Add All Mines
+                </button>
+                <button
+                    className={buttonStyling + "mt-3"}
                     onClick={() => addSingleMine()}
                 >
                     Add Single Mine
-                </button>
-                <button
-                    className={buttonStyling + " mt-3"}
-                    onClick={() => addMine((gridSize * gridSize) / 5)}
-                >
-                    Add All Mines
                 </button>
                 <p className="select-none text-2xl font-medium">
                     Set Grid Size:
@@ -276,9 +299,11 @@ export default function Minesweeper() {
                 >
                     20x20
                 </button>
+
                 <br />
+
                 <Link href={"/"}>
-                    <button className={buttonStyling + " mt-2"}>
+                    <button className={buttonStyling + "mt-2"}>
                         Back To Home
                     </button>
                 </Link>
