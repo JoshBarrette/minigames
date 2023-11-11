47261d0ec228fe35b0dc16195eea5050b5a61b9c
JoshBarrette <joshpbarrette@gmail.com>
2023-11-11 00:03:05 -0600

    Got auto-filling working in Sudoku when pasting long strings of numbers

--- src/app/Sudoku/page.tsx
+++ src/app/Sudoku/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { RefObject, createRef, useRef, useState } from "react";
 
-function RenderGrid(props: {
+function RenderSudokuGrid(props: {
     grid: Array<number>;
     callback: (b: boolean) => void;
     status?: boolean;
+    refsGrid: Array<RefObject<any>>;
 }) {
     return (
         <div className="grid grid-cols-3 gap-2">
@@ -20,7 +21,7 @@ function RenderGrid(props: {
                 let paddingTop = rowNum % 3 === 0 && rowNum !== 0 ? "mt-4" : "";
                 let paddingRight = colNum % 3 !== 2 ? "mr-4" : "";
                 let squareStyles =
-                    "w-10 bg-blue-300 leading-10 text-black text-2xl";
+                    "w-10 bg-blue-300 leading-10 text-black text-2xl text-center";
                 return (
                     <div
                         className={`grid grid-cols-3 gap-2 text-center ${paddingTop} ${paddingRight}`}
@@ -28,21 +29,43 @@ function RenderGrid(props: {
                     >
                         {[arr[index], arr[index + 1], arr[index + 2]].map(
                             (val, subIndex) => (
-                                <p
-                                    className={squareStyles}
-                                    onClick={() => {
-                                        arr[index + subIndex] = -1;
+                                <input
+                                    onChange={(event) => {
                                         if (
-                                            props.callback !== undefined &&
-                                            props.status !== undefined
+                                            (event.target.value === "" &&
+                                                isNaN(
+                                                    event.target.valueAsNumber
+                                                )) ||
+                                            event.target.valueAsNumber < 0
                                         ) {
-                                            props.callback(!props.status);
+                                            arr[index + subIndex] = 0;
                                         }
+
+                                        let num = +event.target.valueAsNumber
+                                            .toString()
+                                            .split("")
+                                            .reverse()
+                                            .join("");
+                                        let i = index + subIndex;
+                                        while (num > 0) {
+                                            arr[i] = num % 10;
+                                            props.refsGrid[i].current.focus();
+                                            i++;
+                                            num = Math.floor(num / 10);
+                                        }
+
+                                        props.callback(!props.status);
                                     }}
+                                    value={
+                                        val !== 0 || isNaN(val)
+                                            ? arr[index + subIndex]
+                                            : ""
+                                    }
+                                    className={squareStyles}
+                                    type="number"
+                                    ref={props.refsGrid[index + subIndex]}
                                     key={index + subIndex}
-                                >
-                                    {val !== 0 ? val : null}
-                                </p>
+                                />
                             )
                         )}
                     </div>
@@ -55,11 +78,18 @@ function RenderGrid(props: {
 export default function Sudoku() {
     const [grid, setGrid] = useState(makeNewGrid());
     const [shouldRefresh, setShouldRefresh] = useState(false);
+    const [refsGrid, setRefsGrid] = useState(makeNewRefsGrid());
 
     function makeNewGrid(): Array<number> {
         return new Array<number | null>(81)
             .fill(null)
             .map((val, index) => index);
+    }
+
+    function makeNewRefsGrid(): Array<RefObject<any>> {
+        return new Array<RefObject<any> | null>(81)
+            .fill(null)
+            .map(() => createRef());
     }
 
     // TODO: y0
@@ -78,10 +108,11 @@ export default function Sudoku() {
                     Sudoku Solver
                 </p>
 
-                <RenderGrid
+                <RenderSudokuGrid
                     grid={grid}
                     callback={setShouldRefresh}
                     status={shouldRefresh}
+                    refsGrid={refsGrid}
                 />
 
                 <div>
